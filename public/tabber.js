25c23ea1b4e1f033e338a62d32e9ddb5a1629f21
CharDale Barnes <differentaccounts@hotmail.com>
2015-03-09 19:40:01 -0500

    Basic javascript behavior working. Switching branch

--- public/tabber.js
+++ public/tabber.js
@@ -1,8 +1,40 @@
-var about = document.getElementById("about").addEventListener('click', function(){
-               display_about(event)}
+var anchors = document.getElementsByTagName('a');
+// var = document.getElementById(' ');
+// var = document.getElementById(' ');
+// var = document.getElementById(' ');
+show_about = anchors[0].addEventListener('click', function(){
+               alert("working!")}
              );
 
-function display_about(event){
-  alert("It worked!");
-               
-};
+show_concerts = anchors[1].addEventListener('click', function(){
+               alert("working!")}
+               // obj2.style.visibility='hidden';
+               // obj2.style.display='none';
+             );
+             
+show_discography = anchors[2].addEventListener('click', function(){
+               alert("working!")}
+             );
+
+
+             // obj.style.visibility='visible';
+             // obj.style.display='inline';
+             // obj2.style.visibility='hidden';
+             // obj2.style.display='none';
+// jquery option
+// $('#nav div').hide();
+// $('#nav div:first').show();
+// $('#nav ul li:first').addClass('active');
+// $('#nav ul li a').click(function(){
+//     var currentTab = $(this).attr('href');
+//     var vis = $(currentTab).is(':visible');
+//     $('#nav div').hide();
+//     $('#nav ul li').removeClass('active');
+//     $(this).parent().addClass('active');
+//     if(vis) {
+//         $(currentTab).hide();
+//     } else {
+//         $(currentTab).show();
+//     }
+// });
+//
